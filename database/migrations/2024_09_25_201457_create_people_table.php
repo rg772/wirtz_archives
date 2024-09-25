495bc8426b137ed61814d9fec8e37cf3be90b12c
7kxRQ9yfN/yQ2Qgh <username@users.noreply.github.com>
2024-09-25 15:38:03 -0500

    the rest

---
+++ database/migrations/2024_09_25_201457_create_people_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string("lastname");
+            $table->string('netid');
+            $table->year('grad');
+            $table->enum('status', ['active', 'deleted', 'hide']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('people');
+    }
+};
